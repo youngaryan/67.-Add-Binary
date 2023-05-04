4d0b508a7f2ec8ea6528b8d63023c3ee138d7659
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-04 16:18:24 +0100

    works however doent work for huge numebrs passes 194/240 tests

--- Solution.java
+++ Solution.java
@@ -1,9 +1,24 @@
+
+
 public class Solution {
     public String addBinary(String a, String b) {
-        int aInt = Integer.parseInt(a,2);
-        int bInt = Integer.parseInt(b,2);
-        int result = aInt+bInt;
+        a = new StringBuilder(a).reverse().toString();
+        b = new StringBuilder(b).reverse().toString();
+        int resultA = 0;
+        int resultB = 0;
 
-        return Integer.toBinaryString(result);
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == '1'){
+                resultA += (int) Math.pow(2,i);
+            }
+        }
+
+        for (int i = 0; i < b.length(); i++) {
+            if (b.charAt(i) == '1') {
+                resultB += (int) Math.pow(2,i);
+            }
+        }
+
+        return Integer.toBinaryString(resultA+resultB);
     }
 }
